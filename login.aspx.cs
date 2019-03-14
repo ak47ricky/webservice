d0dbc8cc7f81c65d86bbcbfbbb1e68a52e8fda22
Unknown <rockymania@gmail.com>
2019-03-14 17:00:11 +0800

    增加判斷Kind前台以及後台。 以及判斷帳號密碼流程

--- login.aspx.cs
+++ login.aspx.cs
@@ -34,16 +34,30 @@ namespace webservice
         {
             string aAccount = string.Empty;
             string aPassword = string.Empty;
+            int aKind = 0;
 
-            string aSqlStr = "select * from accountdata where loginkind = " + Common._ManagerID;
+            string aSqlStr = "select * from dokkansuper.accountdata";
 
             List<AccountData> aListData = new List<AccountData>();
             aListData.Clear();
 
             try
             {
+                //判斷是前台還是後臺登陸
+                aKind = int.Parse(Request.QueryString["kind"].ToString());
+
+                if (aKind == 0)
+                {
+                    Response.Write("1");//登錄代碼錯誤
+                    return;
+                }
+
+                //取得帳號密碼資料
                 aAccount = Request.QueryString["loginname"];
                 aPassword = Request.QueryString["password"];
+
+                if (aKind == 1)//後臺判斷只需要判斷有權限的
+                    aSqlStr = "select * from dokkansuper.accountdata where loginkind in ( 1 , " + Common._ManagerID + ")";
 
                 using (MySqlConnection aCon = new MySqlConnection(ConfigurationManager.ConnectionStrings["MySQL_Admin"].ConnectionString))
                 {
@@ -59,14 +73,15 @@ namespace webservice
                         {
                             aData = new AccountData();
 
+                            //先判斷帳號是否正確
                             if (aAccount != aDR["account"].ToString())
-                            {
-                                Response.Write("1");
-                                return;
-                            }
+                                continue;//不正確直接判斷下一筆
+
+                            //帳號正確後，判斷密碼是否正確
                             if (aPassword != aDR["password"].ToString())
                             {
-                                Response.Write("1");
+                                //不正確就跳錯誤
+                                Response.Write("3");//密碼錯誤
                                 return;
                             }
 
@@ -75,15 +90,24 @@ namespace webservice
                             aData.nickname = aDR["nickname"].ToString();
                             aData.loginkind = int.Parse(aDR["loginkind"].ToString());
 
+                            //如果帳號密碼都符合塞一筆資料就跳出來。
                             aListData.Add(aData);
+                            break;
                         }
+
                         aCon.Close();
                         aDR.Dispose();
+
+                        //判斷是否有找到資料
+                        if (aListData.Count == 0)
+                        {
+                            Response.Write("2");//帳號錯誤
+                            return;
+                        }
 
                         string json_data = JsonConvert.SerializeObject(aListData, Formatting.Indented);
                         Response.Write(json_data);
                     }
-
                 }
             }
             catch
